cba170a11029301fa95eab88e46465168d5e61ad
baebug <ufr134@gmail.com>
2023-01-16 17:52:51 +0900

    init project

---
+++ BE/src/main/java/com/welldone/tonnybunny/TonnybunnyApplication.java
@@ -0,0 +1,13 @@
+package com.welldone.tonnybunny;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TonnybunnyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TonnybunnyApplication.class, args);
+	}
+
+}
---
+++ BE/src/test/java/com/welldone/tonnybunny/TonnybunnyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.welldone.tonnybunny;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TonnybunnyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
