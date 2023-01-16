0488b10bea6589f24f694589c424ec4e41dd21b8
baebug <ufr134@gmail.com>
2023-01-16 21:58:02 +0900

    [BE] feat : 환경변수 파일 추가

---
+++ BE/src/main/java/com/tonnybunny/config/PropertyConfig.java
@@ -0,0 +1,12 @@
+package com.tonnybunny.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
+
+@Configuration
+@PropertySources({
+        @PropertySource("classpath:properties/env.properties")
+})
+public class PropertyConfig {
+}
