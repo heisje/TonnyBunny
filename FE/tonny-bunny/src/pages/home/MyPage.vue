426d0a5a1584c8bea39ba712d38da7814de6b48e
yeomss <dua9920@daum.net>
2023-01-19 17:13:52 +0900

    [FE] fix : 컴포넌트 버그 수정

--- FE/tonny-bunny/src/pages/home/MyPage.vue
+++ FE/tonny-bunny/src/pages/home/MyPage.vue
@@ -1,58 +1,61 @@
 <template>
-	<h1>홈 - 마이 페이지</h1>
+    <h1>홈 - 마이 페이지</h1>
 
-	<div>
-		유저카드
-		<UserCard/>
-		<!-- 유저카드 구성요소
+    <div>
+        유저카드
+        <UserCard />
+        <!-- 유저카드 구성요소
 		프로필이미지아이템
 		정보수정버튼
 		포인트아이템 -->
-	</div>
-	<div v-if="isHelper===true">
-		헬퍼카드
-		<HelperCard/>
-		<!-- 
+    </div>
+    <div v-if="isHelper === true">
+        헬퍼카드
+        <HelperCard />
+        <!-- 
 			자세히보기 XSmallBtn
 			수정하기 XSmallBtn 
 		-->
+    </div>
+    <div>
+        이용내역
+        <LinkBtn />
+        즐겨찾기
+        <LinkBtn />
+        일정
+        <LinkBtn />
+    </div>
 
-	</div>
-	<div>
-		이용내역
-		<LinkBtn/>
-		즐겨찾기
-		<LinkBtn/>
-		일정
-		<LinkBtn/>
-	</div>
-
-	<div>
-		차단목록
-		<MenuItem>
-	</div>
-	<div>
-		푸시알림
-		<MenuItem>
-	</div>
-	<div>
-		고객센터
-		<MenuItem>
-	</div>
-	<div>
-		헬퍼 인증 및 변경
-		<MenuItem>
-	</div>
-	<div>
-		로그아웃
-		<MenuItem>
-	</div>
-
+    <div>
+        차단목록
+        <MenuItem />
+    </div>
+    <div>
+        푸시알림
+        <MenuItem />
+    </div>
+    <div>
+        고객센터
+        <MenuItem />
+    </div>
+    <div>
+        헬퍼 인증 및 변경
+        <MenuItem />
+    </div>
+    <div>
+        로그아웃
+        <MenuItem />
+    </div>
 </template>
 
-
 <script>
-	export default {};
+import MenuItem from "@/components/home/MenuItem.vue";
+
+export default {
+    components: {
+        MenuItem,
+    },
+};
 </script>
 
 <style></style>
