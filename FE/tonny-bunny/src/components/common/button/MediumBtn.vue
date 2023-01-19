f43ea09a5240ddb96aac0ce47252082fd3cb3356
yeomss <dua9920@daum.net>
2023-01-19 14:40:00 +0900

    [FE] feat : medium button

--- FE/tonny-bunny/src/components/common/button/MediumBtn.vue
+++ FE/tonny-bunny/src/components/common/button/MediumBtn.vue
@@ -1,9 +1,33 @@
 <template>
-    <div>중간 버튼</div>
+    <div class="mediumBtn">
+        <div>{{ text }}</div>
+    </div>
 </template>
 
 <script>
-export default {};
+export default {
+    props: {
+        text: {
+            type: String,
+            default: "중간버튼",
+            description: "button tag",
+        },
+    },
+};
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+.mediumBtn {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    color: white;
+    font-size: 0.8rem;
+    font-weight: 600;
+    width: 88px;
+    height: 32px;
+    background: linear-gradient(126.73deg, #ff8a65 15.12%, #ff7765 86.23%);
+    border-radius: 4px;
+}
+</style>
--- FE/tonny-bunny/src/pages/home/HomePage.vue
+++ FE/tonny-bunny/src/pages/home/HomePage.vue
@@ -1,10 +1,17 @@
 <template>
-	<div>홈 - 메인</div>
-	<v-alert type="success"></v-alert>
+    <div>홈 - 메인</div>
+    <v-alert type="success"></v-alert>
+    <MediumBtn />
 </template>
 
 <script>
-	export default {};
+import MediumBtn from "@/components/common/button/MediumBtn.vue";
+
+export default {
+    components: {
+        MediumBtn,
+    },
+};
 </script>
 
 <style></style>
