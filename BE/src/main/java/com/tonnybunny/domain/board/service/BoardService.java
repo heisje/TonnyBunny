4d7d1795b3863f37f9b48480308d0b96cc6cb1d2
baebug <ufr134@gmail.com>
2023-01-19 14:57:00 +0900

    [BE] feat : 게시판 댓글 목업 구현

--- BE/src/main/java/com/tonnybunny/domain/board/service/BoardService.java
+++ BE/src/main/java/com/tonnybunny/domain/board/service/BoardService.java
@@ -1,7 +1,9 @@
 package com.tonnybunny.domain.board.service;
 
 
+import com.tonnybunny.domain.board.dto.BoardCommentRequestDto;
 import com.tonnybunny.domain.board.dto.BoardRequestDto;
+import com.tonnybunny.domain.board.entity.BoardCommentEntity;
 import com.tonnybunny.domain.board.entity.BoardEntity;
 import org.springframework.stereotype.Service;
 
@@ -36,7 +38,8 @@ public class BoardService {
 
 
 	/**
-	 * repository 에서 insertBoard(board) 를 수행한다. JPA 가 board 에 키 값을 넣어줌.
+	 * repository 에서 insertBoard(board) 를 수행한다.
+	 * JPA 가 board 에 키 값을 넣어줌.
 	 *
 	 * @param boardRequestDto
 	 * @return boardSeq
@@ -49,7 +52,8 @@ public class BoardService {
 
 
 	/**
-	 * oldBoard = repository.findBoardBySeq(boardSeq) 를 수행한다. _old 에 _new 를 덮어씌운다.
+	 * oldBoard = repository.findBoardBySeq(boardSeq) 를 수행한다.
+	 * _old 에 _new 를 덮어씌운다.
 	 *
 	 * @param boardSeq
 	 * @param boardRequestDto
@@ -72,6 +76,42 @@ public class BoardService {
 	public Boolean deleteBoard(Long boardSeq) {
 
 		return true;
+	}
+
+
+	/**
+	 * repository 에서 insertBoardComment(boardComment) 를 통해 댓글을 등록한다.
+	 * repository 에서 findBoardBySeq(boardSeq) 를 통해 BoardEntity 를 가져온다.
+	 * BoardEntity 내부의 addComment(boardComment) 를 통해 댓글 정보를 추가한다.
+	 *
+	 * @param boardSeq               : 댓글이 작성되는 게시글의 pk
+	 * @param boardCommentRequestDto : 작성되는 댓글의 정보를 담고 있는 Dto
+	 */
+	public void createBoardComment(Long boardSeq, BoardCommentRequestDto boardCommentRequestDto) {
+		BoardCommentEntity boardComment = boardCommentRequestDto.toEntity();
+	}
+
+
+	/**
+	 * repository 에서 findBoardCommentBySeq(commentSeq) 를 통해 BoardCommentEntity 를 가져온다.
+	 * 내용을 수정한다.
+	 *
+	 * @param commentSeq             : 수정할 댓글의 pk
+	 * @param boardCommentRequestDto : 수정할 댓글의 정보를 담고 있는 Dto
+	 */
+	public void modifyBoardComment(Long commentSeq, BoardCommentRequestDto boardCommentRequestDto) {
+		BoardCommentEntity boardComment = boardCommentRequestDto.toEntity();
+	}
+
+
+	/**
+	 * repository 에서 removeBoardComment(commentSeq) 를 통해 댓글을 삭제한다.
+	 * Entity 내부 로직 필요한가?
+	 *
+	 * @param commentSeq : 삭제할 댓글의 pk
+	 */
+	public void deleteBoardComment(Long commentSeq) {
+
 	}
 
 }
