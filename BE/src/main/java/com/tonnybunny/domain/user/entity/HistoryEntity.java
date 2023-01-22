cdf3c1c370ee3e7583b6afc94ddc9a4ebe675e3d
baebug <ufr134@gmail.com>
2023-01-22 22:00:24 +0900

    [BE] fix : 히스토리 entity pk 생성

--- BE/src/main/java/com/tonnybunny/domain/user/entity/HistoryEntity.java
+++ BE/src/main/java/com/tonnybunny/domain/user/entity/HistoryEntity.java
@@ -5,11 +5,17 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
 
 
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class HistoryEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "history_seq")
+	private Long seq;
+
 }
