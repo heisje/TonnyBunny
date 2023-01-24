c36fc286944dae8709ac74b13b178118dc692d9e
Hello1Robot <hello11robot@gmail.com>
2023-01-24 23:36:09 +0900

    [FE] feat : Notice 관련 컴포넌트 추가
    * 포스트 형태가 동일한 듯 하여 컴포넌트 추가
     #feature/S08P12E105-88 #done

---
+++ FE/tonny-bunny/src/components/notice/NoticePostItem.vue
@@ -0,0 +1,14 @@
+<template>
+    <h1>NoticePostItem</h1>
+</template>
+
+<script>
+export default {
+    name: "NoticePostItem"
+
+}
+</script>
+
+<style>
+
+</style>
--- FE/tonny-bunny/src/pages/notice/NoticePage.vue
+++ FE/tonny-bunny/src/pages/notice/NoticePage.vue
@@ -1,9 +1,18 @@
 <template>
-	<div>고객센터 - 공지사항</div>
+	<h1>고객센터 - 공지사항</h1>
+	
+	공지사항 및 FAQ 공통 게시물 형태
+	<NoticePostItem/>
 </template>
 
 <script>
-	export default {};
+import NoticePostItem from "@/components/notice/NoticePostItem.vue"
+	export default {
+		name: "NoticePage",
+		components:{
+			NoticePostItem,
+		}
+	};
 </script>
 
 <style></style>
