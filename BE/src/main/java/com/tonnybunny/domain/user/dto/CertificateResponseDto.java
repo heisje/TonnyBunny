af5fbd26e6340602a4964dfc8896d3db67393bb1
JCW <codnjs8221@naver.com>
2023-01-24 15:54:24 +0900

    [BE] cleanup : 오타 수정
    S08P12E105-38 #done

--- BE/src/main/java/com/tonnybunny/domain/user/dto/CertificateResponseDto.java
+++ BE/src/main/java/com/tonnybunny/domain/user/dto/CertificateResponseDto.java
@@ -1,24 +1,28 @@
 package com.tonnybunny.domain.user.dto;
 
+
 import com.tonnybunny.domain.user.entity.CertificateEntity;
 import lombok.Data;
 
 import java.util.ArrayList;
 import java.util.List;
 
+
 @Data
 public class CertificateResponseDto {
 
-    public static CertificateResponseDto fromEntity(CertificateEntity certificate){
-        return new CertificateResponseDto();
-    }
+	public static CertificateResponseDto fromEntity(CertificateEntity certificate) {
+		return new CertificateResponseDto();
+	}
 
-    public static List<CertificateResponseDto> fromEntityList(List<CertificateEntity> certificateList){
-        List<CertificateResponseDto> result = new ArrayList<>();
-        for(CertificateEntity certificate : certificateList){
-            CertificateResponseDto scheduleResponseDto = fromEntity(certificate);
-            result.add( scheduleResponseDto );
-        }
-        return result;
-    }
+
+	public static List<CertificateResponseDto> fromEntityList(List<CertificateEntity> certificateList) {
+		List<CertificateResponseDto> result = new ArrayList<>();
+		for (CertificateEntity certificate : certificateList) {
+			CertificateResponseDto certificateResponseDto = fromEntity(certificate);
+			result.add(certificateResponseDto);
+		}
+		return result;
+	}
+
 }
