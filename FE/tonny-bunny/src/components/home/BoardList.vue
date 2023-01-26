de498d21c06e4f33f2aa80ba83352c306f5e7876
yeomss <dua9920@daum.net>
2023-01-26 17:44:50 +0900

    [FE] feat : main page 구현

--- FE/tonny-bunny/src/components/home/BoardList.vue
+++ FE/tonny-bunny/src/components/home/BoardList.vue
@@ -1,16 +1,29 @@
 <template>
-	<h1>BoardList</h1>
-	<BoardListItem />
+    <div class="boardContainer">
+        <div class="boardWrap">
+            <div class="board">
+                <div class="title">
+                    <title-text title="커뮤니티" type="h1-small" top="0" bottom="0"></title-text>
+                </div>
+                <div class="item">
+                    <board-list-item></board-list-item>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
+import TitleText from "../common/TitleText.vue";
 import BoardListItem from "@/components/home/BoardListItem.vue";
 
 export default {
-	name: "BoardList",
-	components: {
-		BoardListItem
-	}
+    name: "BoardList",
+
+    components: {
+        BoardListItem,
+        TitleText,
+    },
 };
 </script>
 
