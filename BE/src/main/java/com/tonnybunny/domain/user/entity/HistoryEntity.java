7da820a46aafd10f10875b357be49977353ad829
SeungtaeRyu <dbtmdxo1992@naver.com>
2023-01-27 01:06:29 +0900

    [BE] feat : 히스토리, 히스토리 상속 구현
    S08P12E105-162 #done

--- BE/src/main/java/com/tonnybunny/domain/user/entity/HistoryEntity.java
+++ BE/src/main/java/com/tonnybunny/domain/user/entity/HistoryEntity.java
@@ -6,9 +6,12 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 
 
 @Entity
+@Inheritance(strategy = InheritanceType.JOINED)
+@DiscriminatorColumn
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class HistoryEntity {
@@ -17,5 +20,26 @@ public class HistoryEntity {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "history_seq")
 	private Long seq;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "client_seq")
+	private UserEntity client;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "helper_seq")
+	private UserEntity helper;
+
+	private String content;
+	private LocalDateTime startDateTime;
+	private LocalDateTime endDateTime;
+
+	private String startLangCode;
+	private String endLangCode;
+	private String taskCode;
+
+	private Long notiSeq;                   // 즉통, 예통의 경우 NotiSeq, 번역의 경우 QuotationSeq
+
+	@Column(name = "review_seq")
+	private Long reviewSeq;
 
 }
