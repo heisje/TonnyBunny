716b6766d15a6a9a6edfe6b1f51f250caafe9ba3
SeungtaeRyu <dbtmdxo1992@naver.com>
2023-01-26 14:40:34 +0900

    [BE] feat : 예약 통역 DTO
    S08P12E105-117 #done

--- BE/src/main/java/com/tonnybunny/domain/ytonny/dto/YTonnyNotiRequestDto.java
+++ BE/src/main/java/com/tonnybunny/domain/ytonny/dto/YTonnyNotiRequestDto.java
@@ -1,12 +1,44 @@
 package com.tonnybunny.domain.ytonny.dto;
 
 
+import com.tonnybunny.domain.user.dto.UserResponseDto;
 import com.tonnybunny.domain.ytonny.entity.YTonnyNotiEntity;
 import lombok.Data;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
 
+
+/**
+ * clientDto                : 고객 seq
+ *
+ * title                    : 제목
+ * estimateDate             : 요청 날짜
+ * estimateStartTime        : 요청 시간
+ * estimateTime             : 예상 소요 시간
+ * estimatePrice            : 의뢰 금액 (null)
+ * content                  : 내용 (null)
+ *
+ * startLangCode            : 시작 언어 코드
+ * endLangCode              : 종료 언어 코드
+ * tonnySituCode            : 통역상황 카테고리 코드 (null)
+ */
 @Data
 public class YTonnyNotiRequestDto {
+
+	private UserResponseDto clientDto;
+
+	private String title;
+	private LocalDate estimateDate;
+	private LocalTime estimateStartTime;
+	private LocalTime estimateTime;
+	private Integer estimatePrice;
+	private String content;
+
+	private String startLangCode;
+	private String endLangCode;
+	private String tonnySituCode;
+
 
 	public YTonnyNotiEntity toEntity() {
 		return (YTonnyNotiEntity) new Object();
