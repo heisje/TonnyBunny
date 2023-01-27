9487b373978e136845057cf4d61f15bea7cec3a7
류승태 <dbtmdxo1992@naver.com>
2023-01-27 10:16:16 +0900

    [BE] feat : 히스토리, 히스토리 상속 구현

--- BE/src/main/java/com/tonnybunny/domain/ytonny/entity/YTonnyHistoryEntity.java
+++ BE/src/main/java/com/tonnybunny/domain/ytonny/entity/YTonnyHistoryEntity.java
@@ -1,50 +1,26 @@
 package com.tonnybunny.domain.ytonny.entity;
 
 
-import com.tonnybunny.common.CommonEntity;
-import com.tonnybunny.domain.review.entity.ReviewEntity;
-import com.tonnybunny.domain.user.entity.UserEntity;
+import com.tonnybunny.domain.user.entity.HistoryEntity;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
-import java.time.LocalDateTime;
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
 import java.time.LocalTime;
 
 
 @Entity
+@DiscriminatorValue("y_tonny")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class YTonnyHistoryEntity extends CommonEntity {
-
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "y_tonny_history_seq")
-	private Long seq;
-
-	@OneToOne // 단방향 1대1 매핑
-	@JoinColumn(name = "review_seq")
-	private ReviewEntity review;
-
-	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "client_seq")
-	private UserEntity client;
-
-	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "helper_seq")
-	private UserEntity helper;
+public class YTonnyHistoryEntity extends HistoryEntity {
 
 	private String title;
-	private LocalDateTime startDateTime;
-	private String description;
 	private LocalTime totalTime;
-	private Integer totalPrice;
+	private Integer unitPrice;
 	private String recordVideoPath;
-
-	private String taskCode;
-	private String startLangCode;
-	private String endLangCode;
-	private String tonnySituCode;
+	private String tonnySituCode = "0";       // 디폴트 "일상" 코드 넣기
 
 }
