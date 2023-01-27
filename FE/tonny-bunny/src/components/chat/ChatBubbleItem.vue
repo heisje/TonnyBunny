de6921c55fc91964d1172898528d949d4d54c76a
heisje <heeje1996@gmail.com>
2023-01-27 11:38:44 +0900

    [FE] feat : 채팅 목업 구현

---
+++ FE/tonny-bunny/src/components/chat/ChatBubbleItem.vue
@@ -0,0 +1,56 @@
+<template lang="">
+    <div>
+        <div v-if="other === true">
+            <div><img src="@/assets/noProfile.png" /></div>
+            <div>{{ name }}</div>
+            <div>{{ text }}</div>
+            <div>{{ time }}</div>
+        </div>
+        <div v-else>
+            <div>{{ text }}</div>
+            <div>{{ time }}</div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name: "ChatBubbleItem",
+    props: {
+        chatData: {
+            type: Object,
+            defaultObject: {
+                name: "a",
+                text: "a",
+                time: "10:00",
+            },
+            description: "상대방",
+        },
+        other: {
+            type: Boolean,
+            default: false,
+            description: "상대방",
+        },
+        imglink: {
+            type: String,
+            default: "@/assets/noProfile.png",
+            description: "이미지",
+        },
+        name: {
+            type: String,
+            default: "이름",
+            description: "이름",
+        },
+        text: {
+            type: String,
+            default: "내용입니다",
+            description: "내용",
+        },
+        time: {
+            type: String,
+            default: "00:00",
+            description: "시간",
+        },
+    },
+};
+</script>
+<style lang=""></style>
