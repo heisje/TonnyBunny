4e461c421e617b7c471629b2f009764c85855df0
JCW <codnjs8221@naver.com>
2023-01-27 15:47:08 +0900

    [BE] feat : 포인트 관리 Dto 작성
    S08P12E105-118 #done

--- BE/src/main/java/com/tonnybunny/domain/point/dto/PointLogResponseDto.java
+++ BE/src/main/java/com/tonnybunny/domain/point/dto/PointLogResponseDto.java
@@ -8,8 +8,23 @@ import java.util.ArrayList;
 import java.util.List;
 
 
+/**
+ * seq          : 포인트 로그 Seq
+ * amount       : 포인트 양
+ * typeCode     : 충전/출금/거래 공통 코드
+ * userSeq      : 포인트 사용자 Seq
+ */
 @Data
 public class PointLogResponseDto {
+
+	private Long seq;
+
+	private Integer amount;
+
+	private String typeCode;
+
+	private Integer userSeq;
+
 
 	public static PointLogResponseDto fromEntity(PointLogEntity pointLog) {
 		return new PointLogResponseDto();
