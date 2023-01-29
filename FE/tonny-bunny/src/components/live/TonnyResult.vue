373629168ff999fd17c07f653dee623af1c60dd4
heeje <heeje1996@gmail.com>
2023-01-29 16:01:57 +0900

    [FE] feat : 즉시통역 헬퍼 페이지 이동 구현
    즉시통역 헬퍼 페이지 이동 구현

--- FE/tonny-bunny/src/components/live/TonnyResult.vue
+++ FE/tonny-bunny/src/components/live/TonnyResult.vue
@@ -8,15 +8,27 @@
         style="width: 300px; height: 300px"
         loop
         autoplay></lottie-player>
-    <img src="@/assets/noProfile.png" alt="" />
-    <div>닉네임</div>
-    <h1>8분 600캐럿</h1>
+
+    <div v-if="isHelper">
+        <img src="@/assets/noProfile.png" alt="" />
+        <div>고객 닉네임님이</div>
+        <h1>8분 600캐럿을 지불하였습니다.</h1>
+    </div>
+    <div v-else>
+        <img src="@/assets/noProfile.png" alt="" />
+        <div>헬퍼 닉네임님의 통역이 종료되었습니다.</div>
+        <h1>8분 600캐럿</h1>
+    </div>
 </template>
 
 <script>
 export default {
     name: "TonnyResultItem",
-
+    data() {
+        return {
+            isHelper: false,
+        };
+    },
     components: {},
 };
 </script>
