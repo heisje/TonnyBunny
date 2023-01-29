bc3bba62f1b61c4fad62b89650902781491c0fb8
heeje <heeje1996@gmail.com>
2023-01-30 00:45:20 +0900

    [FE] feat : 예약통역 클라이언트 페이지이동 구현
    매칭페이지 리다이렉트 제외

--- FE/tonny-bunny/src/pages/ytonny/YTonnyFormPage.vue
+++ FE/tonny-bunny/src/pages/ytonny/YTonnyFormPage.vue
@@ -2,17 +2,50 @@
     <div>
         <title-banner title="🐰 통역예약" text="헬퍼에게 통역 서비스를 예약합니다." />
 
-        <YTonnyClientForm></YTonnyClientForm>
+        <YTonnyClientForm @toggleSubmit="openModal" />
 
-        <div>성공모달</div>
+        <AlarmModal
+            v-show="isOpen"
+            :isOpen="isOpen"
+            title="성공"
+            type="success"
+            btnText2="예"
+            btnColor1="main"
+            btnColor2="carrot"
+            btnFontColor1="white"
+            btnFontColor2="white"
+            @close-modal="closeModal"
+            :to="{ name: 'HistoryPage' }">
+            <template #content>
+                통역예약이 게시되었습니다. <br />
+                헬퍼의 제안 중 가장 좋은 제안을 수락하세요!
+            </template>
+        </AlarmModal>
     </div>
 </template>
 
 <script>
 import YTonnyClientForm from "@/components/ytonny/YTonnyClientForm.vue";
 import TitleBanner from "@/components/common/TitleBanner.vue";
+import AlarmModal from "@/components/common/modal/AlarmModal.vue";
 export default {
-    components: { YTonnyClientForm, TitleBanner },
+    name: "YTonnyFormPage",
+    data() {
+        return {
+            isOpen: false,
+        };
+    },
+    components: { YTonnyClientForm, TitleBanner, AlarmModal },
+    methods: {
+        openModal(e) {
+            e.preventDefault();
+            this.isOpen = true;
+        },
+
+        closeModal() {
+            this.isOpen = false;
+        },
+    },
 };
 </script>
 
