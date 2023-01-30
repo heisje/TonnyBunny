09700177b0bfcb308893c16a10f64b9619a33f90
최권민 <ckm0207@naver.com>
2023-01-30 17:10:45 +0900

    [BE] feat : Follow DTO 작성

--- BE/src/main/java/com/tonnybunny/domain/user/dto/UserRequestDto.java
+++ BE/src/main/java/com/tonnybunny/domain/user/dto/UserRequestDto.java
@@ -9,13 +9,13 @@ import lombok.Data;
  * userCode             : 일반유저, 헬퍼유저 구분하는 공통코드
  * email                : 가입시 입력하는 이메일 (인증 우선순위 낮음)
  * password             : 암호화 과정 위치 결정이 필요함 (프론트/백)
- * [password2]          : 입력 비밀번호 일치 여부를 서버에서도 확인 할 것인가?
+ * [password2]          : 입력 비밀번호 일치 여부를 서버에서도 확인 할것인가?
  * nickname             : 가입을 희망하는 닉네임. 중복 불가능
  * phoneNumber          : 본인 인증을 통과한 전화번호
- *
  * profileImagePath     : 프로필 이미지 경로
  * newPassword          : 변경할 비밀번호
- * [newPassword2]       : 입력 비밀번호 일치 여부를 서버에서도 확인 할 것인가?
+ * [newPassword2]       : 입력 비밀번호
+ * 일치 여부를 서버에서도 확인 할 것인가?
  */
 @Data
 public class UserRequestDto {
@@ -30,12 +30,12 @@ public class UserRequestDto {
 
 	/* 일반 유저 회원정보 수정 */
 	private String profileImagePath = "default.png";
+
 	private String newPassword;
 
 
 	/**
-	 * 1. 빌더패턴으로 작성
-	 * 2. UserEntity 내부의 비즈니스 로직(팩토리 메소드)으로 인스턴스 생성 후 반환
+	 * 1. 빌더패턴으로 작성 2. UserEntity 내부의 비즈니스 로직(팩토리 메소드)으로 인스턴스 생성 후 반환
 	 */
 	public UserEntity toEntity() {
 		return (UserEntity) new Object();
