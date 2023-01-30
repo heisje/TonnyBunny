9974eb0924c41605ba41d54a5f522d07a10267da
heisje <heeje1996@gmail.com>
2023-01-30 17:26:22 +0900

    [FE] fix : axios BaseURL 수정

--- FE/tonny-bunny/src/common/axios.js
+++ FE/tonny-bunny/src/common/axios.js
@@ -2,12 +2,12 @@ import axios from "axios";
 
 // axios 객체 생성 export
 export default axios.create({
-	// 백엔드 localhost:8080
-	baseURL: "http://localhost:8080",
+    // 백엔드 localhost:8080
+    baseURL: "http://localhost:8080/api",
 
-	headers: {
-		"Content-Type": "application/json"
-	},
+    headers: {
+        "Content-Type": "application/json",
+    },
 
-	withCredentials: true
+    withCredentials: true,
 });
