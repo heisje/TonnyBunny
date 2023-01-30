0c9fda3385f8be396d0f9d0b8f5229c4ba2b63d4
yeomss <dua9920@daum.net>
2023-01-30 10:33:21 +0900

    [FE] hotfix: icon bug 수정

--- FE/tonny-bunny/src/App.vue
+++ FE/tonny-bunny/src/App.vue
@@ -1,11 +1,11 @@
 <template>
-	<router-view name="header"></router-view>
-	<main class="container">
-		<router-view></router-view>
-		<bottom-navbar />
-		<login-modal></login-modal>
-	</main>
-	<router-view name="footer"></router-view>
+    <router-view name="header"></router-view>
+    <main class="container">
+        <router-view></router-view>
+        <bottom-navbar />
+        <login-modal></login-modal>
+    </main>
+    <router-view name="footer"></router-view>
 </template>
 
 <script>
@@ -16,31 +16,31 @@ import BottomNavbar from "@/pages/common/BottomNavbar.vue";
 import LoginModal from "./components/common/modal/LoginModal.vue";
 
 export default {
-	name: "App",
+    name: "App",
 
-	components: {
-		BottomNavbar,
-		LoginModal
-	},
+    components: {
+        BottomNavbar,
+        LoginModal,
+    },
 
-	data() {
-		return {
-			tt: 200
-		};
-	},
+    data() {
+        return {
+            tt: 200,
+        };
+    },
 
-	setup() {
-		const store = useStore();
+    setup() {
+        const store = useStore();
 
-		// console.log(store);
-		const counter = computed(() => store.state.counter);
-		const count = store.state.temp.count;
+        // console.log(store);
+        const counter = computed(() => store.state.counter);
+        const count = store.state.temp.count;
 
-		return { counter, count };
-	}
+        return { counter, count };
+    },
 };
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 @import "@/scss/global.scss";
 </style>
