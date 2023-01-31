c0dd2f2cbf2fda6e203470fd90a681201f801d4d
heisje <heeje1996@gmail.com>
2023-01-31 15:55:21 +0900

    [FE] feat : loading 구현
    loading 구현

--- FE/tonny-bunny/src/store/getters.js
+++ FE/tonny-bunny/src/store/getters.js
@@ -3,6 +3,7 @@ import account from "./getters/account";
 import board from "./getters/board";
 import tonny from "./getters/tonny";
 import bunny from "./getters/bunny";
+import common from "./getters/common";
 
 export default {
     // 각 파일 만들어서 모듈화 사용
@@ -11,4 +12,5 @@ export default {
     ...board,
     ...tonny,
     ...bunny,
+    ...common,
 };
