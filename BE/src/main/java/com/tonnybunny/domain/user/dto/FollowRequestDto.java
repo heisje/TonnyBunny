4ce6bf95e388c1edc0a9335dcd6f1b63a0693a0b
최권민 <ckm0207@naver.com>
2023-02-01 03:29:13 +0900

    [BE] feat : 회원가입 및 로그인 형식 및 리턴값 수정

--- BE/src/main/java/com/tonnybunny/domain/user/dto/FollowRequestDto.java
+++ BE/src/main/java/com/tonnybunny/domain/user/dto/FollowRequestDto.java
@@ -5,8 +5,14 @@ import com.tonnybunny.domain.user.entity.FollowEntity;
 import lombok.Data;
 
 
+/**
+ * seq                  : 요청하는 유저의 키 값
+ */
 @Data
 public class FollowRequestDto {
+
+	private Long seq;
+
 
 	public FollowEntity toEntity() {
 		return (FollowEntity) new Object();
