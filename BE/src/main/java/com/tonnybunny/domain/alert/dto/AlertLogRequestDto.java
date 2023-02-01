a5ba27d0786fd21313c728c360a62d7a3ae5b4bd
염정아 <dua9920@daum.net>
2023-02-01 11:59:08 +0900

    [BE] feat : alert service 구현, 수정점 fix

--- BE/src/main/java/com/tonnybunny/domain/alert/dto/AlertLogRequestDto.java
+++ BE/src/main/java/com/tonnybunny/domain/alert/dto/AlertLogRequestDto.java
@@ -1,17 +1,24 @@
 package com.tonnybunny.domain.alert.dto;
 
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.tonnybunny.domain.alert.entity.AlertLogEntity;
 import lombok.Data;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import javax.annotation.PostConstruct;
+
 
 @Data
+@JsonInclude(JsonInclude.Include.NON_NULL) // not null 만 넘겨주도록
 public class AlertLogRequestDto {
 
+	// 의존성 주입에 static 사용하기
+	public static ModelMapper modelMapper = new ModelMapper();
+
 	@Autowired
-	private ModelMapper modelMapper;
+	private ModelMapper modelBeanMapper;
 
 	private int page = 0; // offset
 	private int size = 10; // limit
@@ -21,8 +28,14 @@ public class AlertLogRequestDto {
 	private String content;
 
 
-	public AlertLogEntity toEntity(AlertLogRequestDto alertLogRequestDto) {
+	public static AlertLogEntity toEntity(AlertLogRequestDto alertLogRequestDto) {
 		return modelMapper.map(alertLogRequestDto, AlertLogEntity.class);
+	}
+
+
+	@PostConstruct
+	private void initialize() {
+		modelMapper = this.modelBeanMapper;
 	}
 
 }
