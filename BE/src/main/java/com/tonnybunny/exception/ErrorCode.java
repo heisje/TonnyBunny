629dfc389cd9f8b15e8b7cde56e3e86524cab0fc
JCW <codnjs8221@naver.com>
2023-02-01 11:24:27 +0900

    [BE] feat : 히스토리 내역 Custom Exception 수정
    S08P12E105-203

--- BE/src/main/java/com/tonnybunny/exception/ErrorCode.java
+++ BE/src/main/java/com/tonnybunny/exception/ErrorCode.java
@@ -15,6 +15,7 @@ public enum ErrorCode {
 
 	/* 409 CONFLICT : Resource 의 현재 상태와 충돌. 보통 중복된 데이터 존재 */
 	DUPLICATE_RESOURCE(CONFLICT, "데이터가 이미 존재합니다"),
+	ENTITY_NOT_FOUND(NOT_FOUND, "엔티티가 존재하지 않습니다."),
 	MISMATCH_REQUEST(BAD_REQUEST, "데이터 간 의미가 어긋납니다 (예: 공통코드와 대응되는 데이터)"),
 	;
 
