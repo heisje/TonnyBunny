3dfd21bed5b2c305092be40911a4ec25248bc67a
Kminis <hello11robot@gmail.com>
2023-02-02 10:18:23 +0900

    [BE] cleanup : 에러코드 추가
    - 본인을 차단 / 팔로우 / 신고하지 못하도록 확인하는 에러코드 추가
     #S08P12E105-20 #in_progress

--- BE/src/main/java/com/tonnybunny/exception/ErrorCode.java
+++ BE/src/main/java/com/tonnybunny/exception/ErrorCode.java
@@ -21,7 +21,7 @@ public enum ErrorCode {
 	SIGNUP_INVALIDATION(BAD_REQUEST, "입력된 정보가 지정된 형식과 다릅니다."),
 	LOGIN_BAD_REQUEST(BAD_REQUEST, "로그인 정보가 등록된 정보와 다릅니다."),
 	LACK_OF_POINTS(BAD_REQUEST, "포인트가 부족합니다."),
-
+	SAME_USER_REQUEST(BAD_REQUEST, "요청하는 대상과 요청받는 대상이 동일합니다."),
 	/* 401 UNAUTHORIZED : 권한 인증 문제. JWT 토큰과 관련된 에러 */
 	ACCESS_TOKEN_EXPIRED(UNAUTHORIZED, "토큰의 유효기간이 만료되었습니다."),
 	ACCESS_TOKEN_NOT_FOUND(UNAUTHORIZED, "액세스 토큰이 존재하지 않습니다."),
