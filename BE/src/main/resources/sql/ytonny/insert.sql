e28d524afa8db4709e23b2a53433ea5f5313b6e5
yeomss <dua9920@daum.net>
2023-02-02 17:57:15 +0900

    [BE] feat : ytonny service 구현

--- BE/src/main/resources/sql/ytonny/insert.sql
+++ BE/src/main/resources/sql/ytonny/insert.sql
@@ -5,4 +5,4 @@ values (1, now(), now(), "email1", "nickname1", "pw1", "010", 0, "image", 0, "us
 insert into user_entity 
 values (2, now(), now(), "email2", "nickname2", "pw2", "020", 0, "image2", 0, "usercode2");
 
-select * from ytonny_noti_entity;
+select * from ytonny_entity;
