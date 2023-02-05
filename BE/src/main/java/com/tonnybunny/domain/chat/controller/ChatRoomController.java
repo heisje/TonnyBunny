e2f8e8e89e195ec490741a1f872e608f5cdf9b38
정채원 <codnjs8221@naver.com>
2023-02-05 22:53:35 +0900

    [BE] feat : 채팅 목업 구현 및 DTO 추가

---
+++ BE/src/main/java/com/tonnybunny/domain/chat/controller/ChatRoomController.java
@@ -0,0 +1,50 @@
+package com.tonnybunny.domain.chat.controller;
+
+
+import com.tonnybunny.common.dto.ResultDto;
+import com.tonnybunny.domain.chat.dto.ChatRoomResponseDto;
+import com.tonnybunny.domain.chat.entity.ChatRoomEntity;
+import com.tonnybunny.domain.chat.service.ChatRoomService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@RestController
+@RequiredArgsConstructor
+public class ChatRoomController {
+
+	private final ChatRoomService chatRoomService;
+
+
+	/**
+	 * 해당 유저가 속해있는 채팅방 목록을 반환
+	 * - 채팅방 번호, 안 읽은 채팅 수, 최근 메세지 반환
+	 *
+	 * @param userSeq
+	 * @return
+	 */
+	@PostMapping("/chat/room/{userSeq}")
+	public ResponseEntity<ResultDto<List<ChatRoomResponseDto>>> findRoom(@PathVariable("userSeq") Long userSeq) {
+		List<ChatRoomEntity> chatRoomList = chatRoomService.getChatRoomList(userSeq);
+		List<ChatRoomResponseDto> chatRoomResponseDtoList = new ArrayList<>();
+		for (ChatRoomEntity chatRoom : chatRoomList) {
+			String roomSeq = chatRoom.getSeq();
+			Integer notReadCount = chatRoomService.getNotReadCount(roomSeq, userSeq);
+			String recentMesasge = chatRoomService.getRecentMessage(roomSeq);
+			ChatRoomResponseDto chatRoomResponseDto = ChatRoomResponseDto.builder()
+				.roomSeq(roomSeq)
+				.notReadCount(notReadCount)
+				.recentMessage(recentMesasge)
+				.build();
+			chatRoomResponseDtoList.add(chatRoomResponseDto);
+		}
+		return ResponseEntity.ok(ResultDto.of(chatRoomResponseDtoList));
+	}
+
+}
--- BE/src/main/java/com/tonnybunny/domain/chat/dto/ChatDto.java
+++
@@ -1,5 +0,0 @@
-package com.tonnybunny.domain.chat.dto;
-
-
-public class ChatDto {
-}
