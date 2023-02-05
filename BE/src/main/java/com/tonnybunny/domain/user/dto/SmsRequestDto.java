46f5a956952d4bde1747d4d99598f87db3de0408
최권민 <ckm0207@naver.com>
2023-02-06 03:50:10 +0900

    [BE] feat : 헬퍼 관련 기능 구현

---
+++ BE/src/main/java/com/tonnybunny/domain/user/dto/SmsRequestDto.java
@@ -0,0 +1,25 @@
+package com.tonnybunny.domain.user.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Builder
+public class SmsRequestDto {
+
+	private String type;
+	private String contentType;
+	private String countryCode;
+	private String from;
+	private String content;
+	private List<MessageRequestDto> messages;
+
+}
--- BE/src/main/java/com/tonnybunny/domain/user/entity/CertificateEntity.java
+++ BE/src/main/java/com/tonnybunny/domain/user/entity/CertificateEntity.java
@@ -22,10 +22,24 @@ public class CertificateEntity extends CommonEntity {
 	private String langCode;
 
 	private String certName;
-	private String score;
+	private String content;
+
+	private String isDeleted = "F";
 
 	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "helper_info_seq")
 	private HelperInfoEntity helperInfo;
+
+
+	public CertificateEntity(String certName, String content, HelperInfoEntity helperInfo) {
+		this.certName = certName;
+		this.content = content;
+		this.helperInfo = helperInfo;
+	}
+
+
+	public void deleteCertificate() {
+		this.isDeleted = "T";
+	}
 
 }
