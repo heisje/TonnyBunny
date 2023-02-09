3b3b0f4e4b80fd38b3bb814269166fe530f0292c
SeungtaeRyu <dbtmdxo1992@naver.com>
2023-02-09 11:26:45 +0900

    [FE] feat : 번역 견적서 수정중

--- FE/tonny-bunny/src/components/chat/NBunnyQuotationForm.vue
+++ FE/tonny-bunny/src/components/chat/NBunnyQuotationForm.vue
@@ -6,22 +6,27 @@
                 type="h2"
                 title="고객의 어떤 공고를 해결하시겠어요?"
                 text="해결하고 싶은 공고를 선택해주세요"
-                class="mb-3" />
+                class="mb-3"
+            />
 
             <div class="">
                 <label for=""></label>
                 <DropdownInput
                     :dropdownArray="['아이템1', '아이템2', '아이템3']"
                     placeholder="공고를 선택하세요"
-                    @toggle="(e) => (dropdownValue = e)" />
+                    @toggle="(e) => (dropdownValue = e)"
+                />
             </div>
 
-            <title-text
-                important
-                type="h2"
-                title="언어 선택"
-                text="어떤 언어를 통/번역 하실건가요?"
-                bottom="20" />
+            <title-text type="h2" title="제목" />
+
+            <input type="text" v-model="title" readonly />
+
+            <title-text type="h2" title="내용" />
+
+            <textarea type="textarea" v-model="content" readonly></textarea>
+
+            <title-text type="h2" title="언어 선택" bottom="20" />
 
             <div class="d-flex flex-row w-100 mb-5">
                 <div class="w-100">
@@ -29,7 +34,8 @@
                         class="w-100"
                         :dropdownArray="langCodeList"
                         placeholder="내 언어"
-                        @toggle="(e) => (startLangCode = e)" />
+                        @toggle="(e) => (startLangCode = e)"
+                    />
                 </div>
 
                 <div class="swap">
@@ -40,33 +46,29 @@
                     <DropdownInput
                         :dropdownArray="langCodeList"
                         placeholder="필요 언어"
-                        @toggle="(e) => (endLangCode = e)" />
+                        @toggle="(e) => (endLangCode = e)"
+                    />
                 </div>
             </div>
 
-            <title-text important type="h2" title="카테고리" class="mb-3" />
+            <title-text type="h2" title="카테고리" class="mb-3" />
 
             <DropdownInput
                 class="w120"
                 :dropdownArray="['인쇄물', '아이템2', '아이템3']"
                 placeholder="카테고리 선택"
-                @toggle="(e) => (dropdownValue = e)" />
+                @toggle="(e) => (dropdownValue = e)"
+            />
 
             <title-text
-                important
                 type="h2"
                 title="마감 기한을 설정해주세요"
                 text="번역이 마감되어야할 날짜를 선택해주세요"
-                class="mb-3" />
+                class="mb-3"
+            />
 
             <div class="w120">
-                <input
-                    type="date"
-                    :name="input1.id"
-                    :id="input1.id"
-                    :pattern="input1.pattern"
-                    @input="changeInput"
-                    placeholder="시작일 마감일 선택" />
+                <input type="date" @input="changeInput" placeholder="시작일 마감일 선택" />
             </div>
 
             <title-text
@@ -74,25 +76,19 @@
                 type="h2"
                 title="번역의 금액을 설정해주세요"
                 text="번역 시 받으실 금액을 설정해주세요"
-                class="mb-3" />
+                class="mb-3"
+            />
 
             <div class="d-flex">
                 <div class="w-100">
-                    <input
-                        type="text"
-                        :name="input1.id"
-                        :id="input1.id"
-                        :pattern="input1.pattern"
-                        @input="changeInput"
-                        class="w-100"
-                        placeholder="ex)1000" />
+                    <input type="text" @input="changeInput" class="w-100" placeholder="ex)1000" />
                 </div>
                 <div class="backlabel">
                     <h3>CRT</h3>
                 </div>
             </div>
 
-            <title-text type="h2" title="제목" text="최상단에 노출 될 제목입니다" />
+            <!-- <title-text type="h2" title="제목" text="최상단에 노출 될 제목입니다" />
 
             <input
                 type="number"
@@ -104,7 +100,7 @@
 
             <title-text important type="h2" title="내용" text="번역에 대한 내용을 작성해주세요" />
 
-            <textarea type="textarea" placeholder="내용을 입력해주세요" value="" />
+            <textarea type="textarea" placeholder="내용을 입력해주세요" value="" /> -->
 
             <title-text type="h2" title="[선택] 사진" text="추가 사진을 올려주세요" />
             <input type="file" />
@@ -114,7 +110,8 @@
                 style="width: 100%"
                 text="작성하기"
                 color="main"
-                @click.prevent="submitForm(event)" />
+                @click.prevent="submitForm(event)"
+            />
         </form>
     </div>
 </template>
@@ -129,13 +126,10 @@ export default {
     data() {
         return {
             dropdownValue: "",
-            input1: {
-                id: "input1",
-                value: "",
-                pattern: "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$", // 유효성검사 조건(HTML 용)
-                validate: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/, // 유효성검사 조건(JS 용)
-                notice: "", // 유효성검사 결과 텍스트
-            },
+
+            title: "",
+            content: "",
+
             agreeValue: false,
         };
     },
