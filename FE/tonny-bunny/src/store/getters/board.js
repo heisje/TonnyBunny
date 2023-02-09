611af818d49d4f6d5b69b59808bc3616a8ac73bb
김희제 <koik105@naver.com>
2023-02-10 07:55:47 +0900

    feat : 커뮤니티페이지상세구현

--- FE/tonny-bunny/src/store/getters/board.js
+++ FE/tonny-bunny/src/store/getters/board.js
@@ -1,14 +1,20 @@
 export default {
-	/*
+    /*
         Board
     */
-	getBoardList(state) {
-		return state.board.boardList;
-	},
-	getBoardTotalListCount(state) {
-		return state.board.boardListTotalCount;
-	},
-	getBoardDetail(state) {
-		return state.board.boardDetail;
-	}
+    getBoardList(state) {
+        return state.board.boardList;
+    },
+    getBoardTotalListCount(state) {
+        return state.board.boardListTotalCount;
+    },
+    getBoardDetail(state) {
+        return state.board.boardDetail;
+    },
+    getBoardTotalPages(state) {
+        return state.board.totalPages;
+    },
+    getBoardPageNumber(state) {
+        return state.board.pageNumber;
+    },
 };
