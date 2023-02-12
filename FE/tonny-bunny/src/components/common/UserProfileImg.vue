f642c0f4cfa72244e14d2ff58ccd6913a09e7db8
heisje <heeje1996@gmail.com>
2023-02-12 19:25:06 +0900

    [FE] feat : 이미지 주소 연결

---
+++ FE/tonny-bunny/src/components/common/UserProfileImg.vue
@@ -0,0 +1,30 @@
+<template lang="">
+    <div class="userProfileImgContainer">
+        <!-- <img class="userProfileImg" :src="require('@/assets/newLogo3.png')" alt="" /> -->
+        <img
+            :src="`/images/${profileImagePath}`"
+            onerror="this.onerror=null; this.src='/images/noProfile_white.png';" />
+    </div>
+</template>
+<script>
+export default {
+    props: {
+        profileImagePath: {
+            type: String,
+        },
+    },
+};
+</script>
+<style lang="scss">
+.userProfileImgContainer {
+    width: 100%;
+    aspect-ratio: 1/1;
+
+    .userProfileImg {
+        display: block; /*img default display - inline-block*/
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+    }
+}
+</style>
