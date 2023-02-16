d7e7555acdf359fdd5d63a4b3c4f90aec11ca4f6
baebug <ufr134@gmail.com>
2023-02-17 06:33:07 +0900

    feat : set Timezone

--- BE/src/main/java/com/tonnybunny/TonnybunnyApplication.java
+++ BE/src/main/java/com/tonnybunny/TonnybunnyApplication.java
@@ -6,6 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 
 @SpringBootApplication
 @EnableJpaAuditing
@@ -14,6 +17,12 @@ public class TonnybunnyApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TonnybunnyApplication.class, args);
+	}
+
+
+	@PostConstruct
+	void stz() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
 	}
 
 }
