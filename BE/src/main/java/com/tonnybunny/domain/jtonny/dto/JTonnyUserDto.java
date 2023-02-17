7561dd0dcdb14fc09b5a3cff0957f19eee71a77b
baebug <ufr134@gmail.com>
2023-02-17 10:11:45 +0900

    feat : jTonnyDto 에 프로필 이미지 경로 추가

--- BE/src/main/java/com/tonnybunny/domain/jtonny/dto/JTonnyUserDto.java
+++ BE/src/main/java/com/tonnybunny/domain/jtonny/dto/JTonnyUserDto.java
@@ -6,6 +6,8 @@ import com.tonnybunny.domain.user.entity.UserEntity;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.File;
+
 
 @Data
 @NoArgsConstructor
@@ -14,6 +16,7 @@ public class JTonnyUserDto {
 	private Long seq;
 	private String userCode;
 	private String nickName;
+	private String profileImagePath = File.separator + "noProfile.png";
 	private JTonnyHelperInfoDto helperInfo;
 
 
@@ -21,6 +24,7 @@ public class JTonnyUserDto {
 		this.seq = user.getSeq();
 		this.userCode = user.getUserCode();
 		this.nickName = user.getNickName();
+		this.profileImagePath = user.getProfileImagePath();
 		if (this.userCode.equals(UserCodeEnum.클라이언트.getUserCode())) {
 			this.helperInfo = new JTonnyHelperInfoDto();
 		} else {
